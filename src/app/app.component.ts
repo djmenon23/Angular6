a0f079f06e9ec18b64c56268ce00761ac1806caf
dhananjay <dhananjaymenon23@gmail.com>
2018-12-27 18:15:48 +0530

    Added functionality for signup, login using firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
   onNavigate(feature: string){
   this.loadedFeature = feature;
   }
+
+  ngOnInit() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBlh7JyKf3g3doT4zVmvkY-8TralIuap4o",
+    authDomain: "ng-recipe-book-b6ee0.firebaseapp.com",
+  });
+  }
+
 }
